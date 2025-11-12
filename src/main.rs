936c0a7aa89763082846627266684673273ad7ce
Marcus Rådell <marcus@radell.net>
2025-11-12 19:21:08 +0100

    remove hello world endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 mod features;
 
-use axum::{Router, routing::get};
+use axum::Router;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/", get(|| async { "Hello, World!" }))
-        .nest("/status", features::status::router());
+    let app = Router::new().nest("/status", features::status::router());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
