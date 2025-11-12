fdabcc5e4cb2a00321b3cae60a79e018ff4ee3bf
Marcus Rådell <marcus@radell.net>
2025-11-12 20:04:55 +0100

    setup dotenv with dotenvy

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ use axum::Router;
 
 #[tokio::main]
 async fn main() {
+    dotenvy::dotenv().ok();
+
+    let db_url = std::env::var("DATABASE_URL").unwrap();
+
     let app = Router::new().nest("/status", features::status::router());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
