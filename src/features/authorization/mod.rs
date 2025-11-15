a42544f442cf3f2e8387daca24fecf4965826ae8
Marcus Rådell <marcus@radell.net>
2025-11-15 23:05:47 +0100

    implement env var-based sign in validation

--- src/features/authorization/mod.rs
+++ src/features/authorization/mod.rs
@@ -1,8 +1,9 @@
 use axum::{
-    Router,
+    Form, Router,
     response::{Html, IntoResponse, Redirect},
     routing::{get, post},
 };
+use serde::Deserialize;
 
 async fn sign_in() -> impl IntoResponse {
     Html(
@@ -25,8 +26,23 @@ async fn sign_in() -> impl IntoResponse {
     )
 }
 
-async fn authorize() -> impl IntoResponse {
-    Redirect::to("/client/authorization_callback?code=123")
+#[derive(Deserialize)]
+struct AuthorizeFormData {
+    email: String,
+    password: String,
+}
+
+async fn authorize(
+    Form(body): Form<AuthorizeFormData>,
+) -> Result<Redirect, axum::http::StatusCode> {
+    let expected_email = std::env::var("SIGN_IN_EMAIL").unwrap();
+    let expected_password = std::env::var("SIGN_IN_PASSWORD").unwrap();
+
+    if body.email != expected_email || body.password != expected_password {
+        return Err(axum::http::StatusCode::UNAUTHORIZED);
+    }
+
+    Ok(Redirect::to("/client/authorization_callback?code=123"))
 }
 
 async fn token() -> impl IntoResponse {
