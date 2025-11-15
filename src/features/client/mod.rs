5d101cb02848f8014f6a1ea0b5d0446c5d4fb386
Marcus Rådell <marcus@radell.net>
2025-11-15 19:20:47 +0100

    post towards the token endpoint (stubbed)

--- src/features/client/mod.rs
+++ src/features/client/mod.rs
@@ -1,6 +1,16 @@
 use axum::{Router, response::IntoResponse, routing::get};
 
 async fn authorization_callback() -> impl IntoResponse {
+    let client = reqwest::Client::new();
+
+    let response = client
+        .post("http://localhost:3000/authorization/token")
+        .send()
+        .await
+        .unwrap();
+
+    println!("{response:?}");
+
     "Welcome!"
 }
 
