ff0c42f64005ebc629e7e4463dbe6e0d3233b70b
Marcus Rådell <marcus@radell.net>
2025-11-16 13:09:25 +0100

    break out authorization token function to its own file

--- src/features/authorization/mod.rs
+++ src/features/authorization/mod.rs
@@ -1,19 +1,15 @@
 mod authorize;
 mod sign_in;
+mod token;
 
 use axum::{
     Router,
-    response::IntoResponse,
     routing::{get, post},
 };
-
-async fn token() -> impl IntoResponse {
-    "[[TODO: token string]]"
-}
 
 pub fn router() -> Router {
     Router::new()
         .route("/sign_in", get(sign_in::sign_in))
         .route("/authorize", post(authorize::authorize))
-        .route("/token", post(token))
+        .route("/token", post(token::token))
 }
---
+++ src/features/authorization/token.rs
@@ -0,0 +1,5 @@
+use axum::response::IntoResponse;
+
+pub async fn token() -> impl IntoResponse {
+    "[[TODO: token string]]"
+}
