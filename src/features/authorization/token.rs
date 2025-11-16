9464ea2ba809fcc0661b81df6f32cbcc5b144576
Marcus Rådell <marcus@radell.net>
2025-11-16 20:36:50 +0100

    rename DTOs in authorization token handler

--- src/features/authorization/token.rs
+++ src/features/authorization/token.rs
@@ -2,17 +2,17 @@ use axum::{Json, response::IntoResponse};
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize)]
-pub struct Data {
+pub struct AuthorizationData {
     code: String,
 }
 
 #[derive(Serialize)]
-pub struct Response {
+pub struct ResponseData {
     access_token: String,
 }
 
-pub async fn handler(Json(data): Json<Data>) -> impl IntoResponse {
-    Json(Response {
+pub async fn handler(Json(data): Json<AuthorizationData>) -> impl IntoResponse {
+    Json(ResponseData {
         access_token: format!("TODO: access token based on auth code: {}", data.code),
     })
 }
